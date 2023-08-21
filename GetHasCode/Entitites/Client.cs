cc78adf85d5143a5cc80c839aa15dd134e3800e0
Lucas Almeida <lucalmr12@gmail.com>
2023-08-20 21:10:50 -0300

    Adicionar arquivos de projeto.

---
+++ GetHasCode/Entitites/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHasCode.Entitites
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        //customizar para verificar se existe email e nomes iguais (id,nome,cpf)
+
+        public override bool Equals(object NovoCliente)
+        {//iremos comparar O cliente com outro cliente que sera puxado pelo objeto (parametro)
+            if(!(NovoCliente is Client))
+            {//caso o novo cliente não seja do tipo (classe Client) ira retornar falso
+                return false;
+            }
+            Client other = NovoCliente as Client;
+            //criterio de igualdade sera o email = na hora de instanciar
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+
+              return Email.GetHashCode();  
+
+        }
+
+
+    }
+
+
+
+}
---
+++ GetHasCode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GetHasCode.Entitites;
+
+namespace GetHasCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Client a = new Client { Name = "Lucas", Email = "lucalmr@gmail.com" };
+            Client b = new Client { Name = "Lucas", Email = "lucalmr@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+         
+
+        }
+    }
+}
